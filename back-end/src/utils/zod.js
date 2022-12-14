6d32b3de089a1a24649162b8fffa5e5b26dff0c9
AkashSDas <aakashdas368@gmail.com>
2022-12-14 14:37:28 +0530

    add zod middleware and signup schema

---
+++ back-end/src/utils/zod.js
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+// =============================
+// Auth
+// =============================
+
+export var signupSchema = z.object({
+  email: z.string({ required_error: "Required" }).email({ message: "Invalid" }),
+  password: z
+    .string({ required_error: "Required" })
+    .min(8, "Too short")
+    .max(64, "Too long")
+    .refine((pwd) => /[a-z]{3}/.test(pwd), "Must contain 3 lowercase letters")
+    .refine((pwd) => /[A-Z]{2}/.test(pwd), "Must contain 2 uppercase letters")
+    .refine((pwd) => /[0-9]{2}/.test(pwd), "Must contain 2 numbers")
+    .refine(
+      (pwd) => /[!@#$%^&*()\-__+.]{1}/.test(pwd),
+      "Must contain 1 special character"
+    ),
+});
