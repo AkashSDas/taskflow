1ccff12a7fb3b6e974aeb36825f64ad7f6f657c9
AkashSDas <aakashdas368@gmail.com>
2022-12-14 14:38:06 +0530

    refactor signup route

--- back-end/src/routes/auth.route.js
+++ back-end/src/routes/auth.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import * as z from "../utils/zod.js";
 import { signup } from "../controllers/auth.controller.js";
+import { validateResource } from "../middlewares/zod.middleware.js";
+import { sendErrorResponse } from "../utils/error.js";
+import { handleMiddlewareError } from "../utils/async.js";
 
 var router = Router();
 export default router;
@@ -8,4 +12,9 @@ export default router;
 // Routes
 // ===========================
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  validateResource(z.signupSchema),
+  handleMiddlewareError(signup),
+  sendErrorResponse
+);
