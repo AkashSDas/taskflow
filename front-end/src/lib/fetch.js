a516d45900c5af2bc8e337f6c09b37fbbd9b0049
AkashSDas <aakashdas368@gmail.com>
2022-12-15 12:56:45 +0530

    refactor fetch and services

--- front-end/src/lib/fetch.js
+++ front-end/src/lib/fetch.js
@@ -1,9 +1,16 @@
-export async function fetchFromAPI(URL, options) {
+export async function fetchFromAPI(URL, method, data, accessToken) {
+  if (!URL || !method) throw new Error("URL and method are required");
+
   try {
-    let response = await fetch(
-      `${import.meta.env.BACKEND_URL}/${URL}`,
-      options
-    );
+    let response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/${URL}`, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: data && JSON.stringify(data),
+      credentials: "include",
+    });
     var data = await response.json();
     var success = response.status >= 200 && response.status < 300;
   } catch (err) {
--- front-end/src/services/auth.js
+++ front-end/src/services/auth.js
@@ -1,11 +1,25 @@
 import { fetchFromAPI } from "../lib/fetch";
 
-function fetchFromAuth(URL, options) {
-  return fetchFromAPI(`/auth/${URL}`, options);
+function fetchFromAuth(URL, method, data, accessToken) {
+  return fetchFromAPI(`auth/${URL}`, method, data, accessToken);
 }
 
 export async function getNewAccessToken() {
-  var response = await fetchFromAuth("access-token", { method: "GET" });
+  var response = await fetchFromAuth("access-token", "get");
+  if (response.status == 200) {
+    return {
+      success: response.success,
+      user: response.data.user,
+      accessToken: response.data.accessToken,
+    };
+  }
+
+  return { success: response.success, error: response.error };
+}
+
+export async function login(data) {
+  var response = await fetchFromAuth("login", "post", data);
+
   if (response.status == 200) {
     return {
       success: response.success,
