fe5ea31264d37ccfdc3f78bf6516ba026d9af5ce
AkashSDas <aakashdas368@gmail.com>
2022-12-16 11:16:32 +0530

    customize 404 page

--- front-end/src/pages/404.jsx
+++ front-end/src/pages/404.jsx
@@ -1,9 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image, VStack, Text } from "@chakra-ui/react";
+import { chakraTheme, pxToRem } from "../lib/chakra-ui";
 
 export default function NotFoundPage() {
   return (
     <Box>
-      <h1>404</h1>
+      <VStack w="full">
+        <Image
+          src="https://media.giphy.com/media/2LNbVZrfm05G/giphy.gif"
+          h={pxToRem(180)}
+          w={pxToRem(300)}
+          rounded="md"
+          objectFit="cover"
+        />
+
+        <Text color={chakraTheme.color.text2} fontFamily="heading">
+          Page Not Found
+        </Text>
+      </VStack>
     </Box>
   );
 }
